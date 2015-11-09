bc9d06225d2896c9a7e708071a15c103697b2eed
lrnzcig <lrnzcig@gmail.com>
2015-11-09 08:36:45 +0100

    error if not connected to the database

--- almadraba-server/src/main/java/com/sisifo/almadraba_server/AlmadrabaContextListener.java
+++ almadraba-server/src/main/java/com/sisifo/almadraba_server/AlmadrabaContextListener.java
@@ -9,20 +9,33 @@ import com.sisifo.almadraba_server.hbm.HibernateUtil;
 
 public class AlmadrabaContextListener implements ServletContextListener {
 	
-
+	// TODO put this as a startup argument?
+	private static boolean errorIfNotConnected = true;
 	private static SessionFactory factory;
 
 	@Override
-	public void contextDestroyed(ServletContextEvent arg0) {
+	public void contextDestroyed(final ServletContextEvent arg0) {
+		if (factory == null && ! errorIfNotConnected) {
+			return;
+		}
 		factory.close();
 	}
 
 	@Override
-	public void contextInitialized(ServletContextEvent arg0) {
-		factory = HibernateUtil.getSessionFactory();		
+	public void contextInitialized(final ServletContextEvent arg0) {
+		try { 
+			factory = HibernateUtil.getSessionFactory();
+		} catch (ExceptionInInitializerError e) {
+			if (errorIfNotConnected) {
+				throw e;
+			}
+		}
 	}
 
 	public static SessionFactory getSessionFactory() {
+		if (factory == null && ! errorIfNotConnected) {
+			return null;
+		}
 		return factory;
 	}
 
